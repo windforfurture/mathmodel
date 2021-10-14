2bb5ea4d826df0b3414547f1828ed231e89b7c40
yuanli <374702739@qq.com>
2021-10-14 11:59:14 +0800

    1

--- question_1.py
+++ question_1.py
@@ -3,45 +3,39 @@ import pandas as pd
 from sklearn import metrics
 #read_data
 
-class MDS(object):
-    def __init__(self):
-        pass
-    def calculate_distance(self,x, y):
-        d = np.sqrt(np.sum((x - y) ** 2))
-        return d
-    # 计算矩阵各行之间的欧式距离；x矩阵的第i行与y矩阵的第0-j行继续欧式距离计算，构成新矩阵第i行[i0、i1...ij]
-    def calculate_distance_matrix(self,x, y):
-        d = metrics.pairwise_distances(x, y)
-        return d
-    def cal_B(sefl,D):
-        (n1, n2) = D.shape
-        DD = np.square(D)                    # 矩阵D 所有元素平方
-        Di = np.sum(DD, axis=1) / n1         # 计算dist(i.)^2
-        Dj = np.sum(DD, axis=0) / n1         # 计算dist(.j)^2
-        Dij = np.sum(DD) / (n1 ** 2)         # 计算dist(ij)^2
-        B = np.zeros((n1, n1))
-        for i in range(n1):
-            for j in range(n2):
-                B[i, j] = (Dij + DD[i, j] - Di[i] - Dj[j]) / (-2)   # 计算b(ij)
-        return B
-    def MDS(self,data, n=2):
-        D = self.calculate_distance_matrix(data, data)
-        # print(D)
-        B = self.cal_B(D)
-        Be, Bv = np.linalg.eigh(B)             # Be矩阵B的特征值，Bv归一化的特征向量
-        # print numpy.sum(B-numpy.dot(numpy.dot(Bv,numpy.diag(Be)),Bv.T))
-        Be_sort = np.argsort(-Be)
-        Be = Be[Be_sort]                          # 特征值从大到小排序
-        Bv = Bv[:, Be_sort]                       # 归一化特征向量
-        Bez = np.diag(Be[0:n])                 # 前n个特征值对角矩阵
-        # print Bez
-        Bvz = Bv[:, 0:n]                          # 前n个归一化特征向量
-        Z = np.dot(np.sqrt(Bez), Bvz.T).T
-        # print(Z)
-        return Z
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
 
+#
 
+class Analysis_of_variance(object):
+    def __init__(self,x,y,k_th=20):
+        self.x, self.y = x, y
+        selector = SelectKBest(score_func=f_regression, k=k_th)
+        print(x)
+        print(y)
+        results = selector.fit(x, y)
 
+        self.results = results
+
+        # 查看每个特征的分数和p-value
+        # results.scores_: 每个特征的分数
+        # results.pvalues_: 每个特征的p-value
+        # results.get_support(): 返回一个布尔向量，True表示选择该特征，False表示放弃，也可以返回索引
+        self.features = pd.DataFrame({
+            "feature": x.columns,
+            "score": results.scores_,
+            "pvalue": results.pvalues_,
+            "select": results.get_support()
+        })
+        self.features.sort_values("score", ascending=False)
+    def return_features(self):
+        return self.features
+    def return_index(self):
+        x_new_index = self.results.get_support(indices=True)
+        x_new = self.x.iloc[:, x_new_index]
+        x_new.head()
+        print(x_new)
 
 
 
@@ -49,15 +43,13 @@ class MDS(object):
 
 if __name__=='__main__':
     Molecular = pd.read_excel('./data/Molecular_Descriptor.xlsx')
+    ER = pd.read_excel('./data/ERα_activity.xlsx')
     data=Molecular.values
-    data=data[1:-1]
-    for i,values in enumerate(data):
-        print(len(values))
+    data=data[:,1:]
+    label = ER.values[:,2]
+    Analysis_of_variance(data,label)
 
 
-
-
-print("获取到所有的值:\n{}".format(data))
 
 
 
